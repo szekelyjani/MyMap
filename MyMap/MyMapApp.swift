41f9e63bfdd7084aab8564a9d4eab429cae89cc3
János Székely <szekelyjani@gmail.com>
2024-04-15 09:27:32 +0200

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by János Székely on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
